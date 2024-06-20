71e4314a4b0bdfb86e61b72670f2a6ebbd0d3cf5
NameNaman <146347104+Naman85Jain@users.noreply.github.com>
2024-06-20 19:04:27 +0530

    Add files via upload

---
+++ Student_Management_System/src/student/management/system/About.java
@@ -0,0 +1,48 @@
+package student.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class About extends JFrame {
+
+    About() {
+        setSize(700, 500);
+        setLocation(400, 150);
+        getContentPane().setBackground(Color.WHITE);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/About.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(350, 0, 300, 200);
+        add(image);
+
+        JLabel heading = new JLabel("<html>Atharva Group of Institutes<br/>Management System</html>");
+        heading.setBounds(70, 20, 300, 130);
+        heading.setFont(new Font("Tahoma", Font.BOLD, 30));
+        add(heading);
+
+        JLabel name = new JLabel("Developed By: Naman Jain");
+        name.setBounds(70, 220, 550, 40);
+        name.setFont(new Font("Tahoma", Font.BOLD, 30));
+        add(name);
+
+        JLabel rollno = new JLabel("Branch: FE-ECS");
+        rollno.setBounds(70, 280, 550, 40);
+        rollno.setFont(new Font("Tahoma", Font.PLAIN, 30));
+        add(rollno);
+
+        JLabel contact = new JLabel("Contact: namanjain-ecs@atharvacoe.ac.in");
+        contact.setBounds(70, 340, 550, 40);
+        contact.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        add(contact);
+
+        setLayout(null);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new About();
+    }
+}
---
+++ Student_Management_System/src/student/management/system/Conn.java
@@ -0,0 +1,20 @@
+package student.management.system;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///schoolmanagementsystem", "root", "root");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
